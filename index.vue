5e22507e0647e2b5d86637584de0f954599b58bf
凍空希羽 <101105708+itezora-nonoha@users.noreply.github.com>
2022-08-08 02:31:54 +0900

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <h1>記事一覧</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        id: {{ post.id }}<br>
+        slug: {{ post.slug }}<br>
+        title: {{ post.title.rendered }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({$axios}) {
+      const posts = await $axios.$get('http://wordpress/wp-json/wp/v2/posts')
+      return { posts }
+    }
+  }
+</script>
